72106cd248fb3978b27921fde734879b4a651ef4
rullah0202 <rullah0202@gmail.com>
2024-12-23 18:16:34 +0600

    using_promise_then

--- src/components/User.vue
+++ src/components/User.vue
@@ -5,6 +5,9 @@ import { useName } from "@/composables/name.js";
 
  let url = "https://jsonplaceholder.typicode.com/posts/1";
  const { data, error } = useFetch(url);
+ function changeUrl(){
+    url = "https://dummyjson.com/product/1";
+ }
 </script>
 
 <template>
@@ -14,6 +17,7 @@ import { useName } from "@/composables/name.js";
 <div v-if="error">Oops!Error encounted {{ error }}</div>
 <div v-else-if="data">Data Loaded: <pre>{{ data }}</pre> </div>
 <div v-else> <h1>...Loading</h1></div>
+<button @click="changeUrl">Change URL</button>
 </template>
 
 <style scoped>
--- src/composables/fetch.js
+++ src/composables/fetch.js
@@ -16,3 +16,14 @@ export function useFetch(url){
     getData();
     return { data, error };
 }
+//Using Promise then
+// export function useFetch(url){
+//     const data = ref(null);
+//     const error = ref(null);
+//     fetch(url)
+//         .then((res) => res.json())
+//         .then((json) => (data.value = json))
+//         .catch((err) => (error.value = err))
+
+//     return { data, error };
+// }
